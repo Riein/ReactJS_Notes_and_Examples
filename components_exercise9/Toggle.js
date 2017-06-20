ef219be9fbc7e16ea7d19747821f6a4f4ab2b5e0
Sasha Demyanik <ttsvan@msn.com>
2017-06-19 17:03:56 -0700

    Adds component exercise 9
    This exercise aims to practice toggling states of a component class.

---
+++ components_exercise9/Toggle.js
@@ -0,0 +1,32 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+var green = '#009966';
+var yellow = '#FFD712';
+
+var Toggle = React.createClass({
+  getInitialState: function() {
+    return {color: green};
+  },
+  
+  changeColor: function() {
+    var newColor = this.state.color == green ? yellow : green;
+    this.setState({ color: newColor });
+  },
+  
+  render: function () {
+    return (
+      <div style={{background: this.state.color}}>
+        <h1>
+          Change my color
+        </h1>
+        <button onClick={this.changeColor}>
+          Change color
+        </button>
+      </div>
+    );
+  }
+});
+
+ReactDOM.render(<Toggle />, document.getElementById('app'));
