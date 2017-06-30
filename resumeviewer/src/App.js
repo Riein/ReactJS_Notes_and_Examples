19247885f46427fb13609451f5c60292dc7a2f72
Sasha Demyanik <ttsvan@msn.com>
2017-06-30 15:05:12 -0700

    Adds basic JS code to components.
    Adds starter code to the JS components and puts them in order in the
    App.js

--- resumeviewer/src/App.js
+++ resumeviewer/src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Components/Header';
+import About from './Components/About';
+import Resume from './Components/Resume';
+import Portfolio from './Components/Portfolio';
+import Testimonials from './Components/Testimonials';
+import Contacts from './Components/Contacts';
+import Footer from './Components/Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        My App
+        <Header />
+        <About />
+        <Resume />
+        <Portfolio />
+        <Testimonials />
+        <Contacts />
+        <Footer />
       </div>
     );
   }
