1b39d7112cf0088a764668afd15c9a4c7b4b840e
Sasha Demyanik <ttsvan@msn.com>
2017-07-05 20:56:58 -0700

    Passes json data into header.
    Passes relevant header data into header js file and replaces text with
    proper objects.

--- resumeviewer/src/App.js
+++ resumeviewer/src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
+        <Header data={this.state.resumeData.main} />
         <About />
         <Resume />
         <Portfolio />
